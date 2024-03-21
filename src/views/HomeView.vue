d180a308013901ff217881c0ed1c5dc6e01829f0
pablobbb <38733636+pablobbb@users.noreply.github.com>
2024-03-21 22:52:00 +0100

    refactor: rename main files

--- src/views/HomeView.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-</script>
-
-<template>
-  <main>
-    <h1>pablobbb</h1>
-  </main>
-</template>
---
+++ src/views/home.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <h1>Hi! I'm Pablo Bonucci, Web Developer and technical leader with over 10 years of experience, specialized in crafting advanced web applications and optimizing development processes.</h1>
+  </main>
+</template>
