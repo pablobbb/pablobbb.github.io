1827770a65010c2f3634145f2854e07dd0acef29
pablobbb <38733636+pablobbb@users.noreply.github.com>
2024-03-24 22:04:48 +0100

    feat: add some real content on main

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,17 +1,41 @@
 
 <template>
     <section>
-      <h1>Pablo Bonucci<small>Senior Frontend Developer</small></h1>
-      <p>I'm a Web Developer with over 10 years of experience, specialized in crafting advanced web applications and optimizing development processes. I primarily work with TypeScript on Vue.js framework but am open to other frameworks and programming languages.<br><br>My career started as a Designer, but it soon became clear that my true passion was Development. I love combining both skillsets to create products that fulfill a clear purpose.</p>
+      <h1>Hi! 👋 I'm Pablo <small>Senior Frontend Developer</small></h1>
+      <p>I'm a Frontend Developer specialized in crafting advanced web applications and optimizing development processes.</p>
     </section>
     <section>
       <h2>About me</h2>
     </section>
     <section>
       <h2>Skills</h2>
+      <div class="squares skills">
+        <span>Skill 1</span>
+        <span>Skill 2</span>
+        <span>Skill 3</span>
+        <span>Skill 4</span>
+        <span>Skill 5</span>
+        <span>Skill 6</span>
+        <span>Skill 7</span>
+        <span>Skill 8</span>
+        <span>Skill 9</span>
+        <span>Skill 10</span>
+      </div>
     </section>
     <section>
       <h2>Tech Stack</h2>
+      <div class="squares tech-stack">
+        <span>Tech Stack 1</span>
+        <span>Tech Stack 2</span>
+        <span>Tech Stack 3</span>
+        <span>Tech Stack 4</span>
+        <span>Tech Stack 5</span>
+        <span>Tech Stack 6</span>
+        <span>Tech Stack 7</span>
+        <span>Tech Stack 8</span>
+        <span>Tech Stack 9</span>
+        <span>Tech Stack 10</span>
+      </div>
     </section>
     <section>
       <h2>Experience</h2>
