bcabdb16bc755a8ff967403ce878b26579c00428
pablobbb <38733636+pablobbb@users.noreply.github.com>
2024-03-25 23:12:17 +0100

    feat: use fa icons on header nav

--- src/app.vue
+++ src/app.vue
@@ -7,9 +7,9 @@ import { RouterLink, RouterView } from 'vue-router'
     <nav>
       <div class="logo"><RouterLink class="home" to="/"><span>&lt;</span>pablobbb<span>/&gt;</span></RouterLink></div>
       <div class="social">
-        <a href="https://www.linkedin.com/in/pablobonucci" target="_blank" alt="LinkedIn"><img class="linkedin" src="/src/assets/icon-linkedin.svg"></a>
-        <a href="https://twitter.com/fresh_rapp" target="_blank" alt="X"><img src="/src/assets/icon-x.svg"></a>
-        <a href="https://github.com/pablobbb" target="_blank" alt="GitHub"><img src="/src/assets/icon-github.svg"></a>
+        <a href="https://www.linkedin.com/in/pablobonucci" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
+        <a href="https://twitter.com/fresh_rapp" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
+        <a href="https://github.com/pablobbb" target="_blank"><i class="fa-brands fa-github"></i></a>
       </div>
     </nav>
   </header>
