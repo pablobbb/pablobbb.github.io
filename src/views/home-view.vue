65d9c05dfff70530945fa2918b1db142a89d0864
pablobbb <38733636+pablobbb@users.noreply.github.com>
2024-03-27 08:00:52 +0100

    refactor: cleanup home-view for v1

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,43 +1,10 @@
 
 <template>
     <section>
-      <h1>Hi! 👋 I'm Pablo <small>Senior Frontend Developer</small></h1>
-      <p>I'm a Frontend Developer specialized in crafting advanced web applications and optimizing development processes.</p>
-    </section>
-    <section>
-      <h2>About me</h2>
-    </section>
-    <section>
-      <h2>Skills</h2>
-      <div class="squares skills">
-        <span>Skill 1</span>
-        <span>Skill 2</span>
-        <span>Skill 3</span>
-        <span>Skill 4</span>
-        <span>Skill 5</span>
-        <span>Skill 6</span>
-        <span>Skill 7</span>
-        <span>Skill 8</span>
-        <span>Skill 9</span>
-        <span>Skill 10</span>
+      <div class="welcome">
+      <h1>Hi! 👋 I'm Pablo <small>Senior Frontend Developer @ QubicaAMF Europe</small></h1>
+      <p>I'm a Web Developer specialized in crafting advanced applications and optimizing development processes. I actively strive to improve the code quality and performance of the software I work on, without neglecting the Developer Experience so sought after by (us) developers.
+        <br><br>I am an advocate of Agile Methodologies to facilitate Continuous Integration / Continuous Delivery (CI/CD) for complex projects that are constantly evolving.</p>
       </div>
-    </section>
-    <section>
-      <h2>Tech Stack</h2>
-      <div class="squares tech-stack">
-        <span>Tech Stack 1</span>
-        <span>Tech Stack 2</span>
-        <span>Tech Stack 3</span>
-        <span>Tech Stack 4</span>
-        <span>Tech Stack 5</span>
-        <span>Tech Stack 6</span>
-        <span>Tech Stack 7</span>
-        <span>Tech Stack 8</span>
-        <span>Tech Stack 9</span>
-        <span>Tech Stack 10</span>
-      </div>
-    </section>
-    <section>
-      <h2>Experience</h2>
-    </section>
+      </section>
 </template>
