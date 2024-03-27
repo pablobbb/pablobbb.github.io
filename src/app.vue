3bc6ba5d417dcabcaf4f628f11e927d505bdb5e3
pablobbb <38733636+pablobbb@users.noreply.github.com>
2024-03-27 08:54:06 +0100

    style: run prettier format

--- src/app.vue
+++ src/app.vue
@@ -5,18 +5,24 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav>
-      <div class="logo"><RouterLink class="home" to="/"><span>&lt;</span>pablobbb<span>/&gt;</span></RouterLink></div>
+      <div class="logo">
+        <RouterLink class="home" to="/"><span>&lt;</span>pablobbb<span>/&gt;</span></RouterLink>
+      </div>
       <div class="social">
-        <a href="https://www.linkedin.com/in/pablobonucci" target="_blank"><i class="fa-brands fa-linkedin-in"></i></a>
-        <a href="https://twitter.com/fresh_rapp" target="_blank"><i class="fa-brands fa-x-twitter"></i></a>
-        <a href="https://github.com/pablobbb" target="_blank"><i class="fa-brands fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/pablobonucci" target="_blank"
+          ><i class="fa-brands fa-linkedin-in"></i
+        ></a>
+        <a href="https://twitter.com/fresh_rapp" target="_blank"
+          ><i class="fa-brands fa-x-twitter"></i
+        ></a>
+        <a href="https://github.com/pablobbb" target="_blank"
+          ><i class="fa-brands fa-github"></i
+        ></a>
       </div>
     </nav>
   </header>
   <main>
     <RouterView />
   </main>
-  <footer>
-    &copy; 2024
-  </footer>
+  <footer>&copy; 2024</footer>
 </template>
--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -1,10 +1,14 @@
-
 <template>
-    <section>
-      <div class="welcome">
+  <section>
+    <div class="welcome">
       <h1>Hi! 👋 I'm Pablo <small>Senior Frontend Developer @ QubicaAMF Europe</small></h1>
-      <p>I'm a Web Developer specialized in crafting advanced applications and optimizing development processes. I actively strive to improve the code quality and performance of the software I work on, without neglecting the Developer Experience so sought after by (us) developers.
-        <br><br>I am an advocate of Agile Methodologies to facilitate Continuous Integration / Continuous Delivery (CI/CD) for complex projects that are constantly evolving.</p>
-      </div>
-      </section>
+      <p>
+        I'm a Web Developer specialized in crafting advanced applications and optimizing development
+        processes. I actively strive to improve the code quality and performance of the software I
+        work on, without neglecting the Developer Experience so sought after by (us) developers.
+        <br /><br />I am an advocate of Agile Methodologies to facilitate Continuous Integration /
+        Continuous Delivery (CI/CD) for complex projects that are constantly evolving.
+      </p>
+    </div>
+  </section>
 </template>
