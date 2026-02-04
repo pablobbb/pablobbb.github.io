d77447e978919fae9f983cd6ad43117643eedae0
pablobbb <38733636+pablobbb@users.noreply.github.com>
2026-02-04 15:06:59 +0100

    feat: update job position

--- src/views/home-view.vue
+++ src/views/home-view.vue
@@ -5,7 +5,7 @@
   >
     <section>
       <div class="welcome">
-        <h1>Hi! <span class="wave">👋</span> I'm Pablo <small>Lead Frontend Engineer @ iubenda</small></h1>
+        <h1>Hi! <span class="wave">👋</span> I'm Pablo <small>Principal Frontend Engineer @ iubenda</small></h1>
         <p>
           I'm a Web Developer who builds advanced apps and makes development work better. I'm really passionate about creating high-quality software, while making sure it's still enjoyable for developers to work with! <br /><br />I strongly believe in Agile methods to help with Continuous Integration /
           Continuous Delivery (CI/CD) for complex projects that need to evolve and change over time. I love improving code quality and optimizing processes.
